22477e9df61789f097c3c2bdde5c974421fe127a
Higor Araújo <higor.araujo@virtus.ufcg.edu.br>
2022-11-30 10:10:03 -0300

    fix: fix lint errors

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -5,75 +5,95 @@ import { UserService } from '../services/userService';
 const userServiceInstance = new UserService();
 
 export class UserController {
-  async createUser (request: Request, response: Response): Promise<Response> {
+  async createUser(request: Request, response: Response): Promise<Response> {
     try {
       const user = await userServiceInstance.createUser({
-        ...request.body as IUser
+        ...(request.body as IUser)
       });
       return response.status(StatusCodes.OK).send(user);
     } catch (err) {
       if (err instanceof Error) {
-        return response.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: err.message });
+        return response
+          .status(StatusCodes.INTERNAL_SERVER_ERROR)
+          .json({ error: err.message });
       }
       const errorMessage = 'Failed to do something exceptional';
-      return response.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: errorMessage });
+      return response
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .json({ error: errorMessage });
     }
   }
 
-  async getUser (request: Request, response: Response): Promise<Response> {
+  async getUser(request: Request, response: Response): Promise<Response> {
     try {
       const id = request.params.id;
       const user = await userServiceInstance.getUserById(Number(id));
       return response.status(StatusCodes.OK).send(user);
     } catch (err) {
       if (err instanceof Error) {
-        return response.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: err.message });
+        return response
+          .status(StatusCodes.INTERNAL_SERVER_ERROR)
+          .json({ error: err.message });
       }
       const errorMessage = 'Failed to do something exceptional';
-      return response.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: errorMessage });
+      return response
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .json({ error: errorMessage });
     }
   }
 
-  async getAllUsers (request: Request, response: Response): Promise<Response> {
+  async getAllUsers(request: Request, response: Response): Promise<Response> {
     try {
       const users = await userServiceInstance.getAllUsers();
       return response.status(StatusCodes.OK).send(users);
     } catch (err) {
       if (err instanceof Error) {
-        return response.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: err.message });
+        return response
+          .status(StatusCodes.INTERNAL_SERVER_ERROR)
+          .json({ error: err.message });
       }
       const errorMessage = 'Failed to do something exceptional';
-      return response.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: errorMessage });
+      return response
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .json({ error: errorMessage });
     }
   }
 
-  async updateUser (request: Request, response: Response): Promise<Response> {
+  async updateUser(request: Request, response: Response): Promise<Response> {
     try {
       const id = request.params.id;
       const updatedUser = await userServiceInstance.updateUser(Number(id), {
-        ...request.body as IUser
+        ...(request.body as IUser)
       });
       return response.status(StatusCodes.OK).send(updatedUser);
     } catch (err) {
       if (err instanceof Error) {
-        return response.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: err.message });
+        return response
+          .status(StatusCodes.INTERNAL_SERVER_ERROR)
+          .json({ error: err.message });
       }
       const errorMessage = 'Failed to do something exceptional';
-      return response.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: errorMessage });
+      return response
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .json({ error: errorMessage });
     }
   }
 
-  async deleteUser (request: Request, response: Response): Promise<Response> {
+  async deleteUser(request: Request, response: Response): Promise<Response> {
     try {
       const id = request.params.id;
       const deletedUser = await userServiceInstance.deleteUser(Number(id));
       return response.status(StatusCodes.OK).send(deletedUser);
     } catch (err) {
       if (err instanceof Error) {
-        return response.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: err.message });
+        return response
+          .status(StatusCodes.INTERNAL_SERVER_ERROR)
+          .json({ error: err.message });
       }
       const errorMessage = 'Failed to do something exceptional';
-      return response.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: errorMessage });
+      return response
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .json({ error: errorMessage });
     }
   }
 }
