e77e61d2f5e2e0d6b8d8984849b214644959dde3
Vinicius Lacerda <jose.arruda@profusion.mobi>
2023-01-25 23:55:48 -0300

    feat: create invites and add them to groups

--- src/routes/group.routes.ts
+++ src/routes/group.routes.ts
@@ -11,8 +11,13 @@ groupRoutes.post('/groups', authMiddleware, groupControllerInstance.createGroup)
 groupRoutes.get('/groups/:id', authMiddleware, groupControllerInstance.getGroup);
 groupRoutes.get('/groups', authMiddleware, groupControllerInstance.getAllGroups);
 groupRoutes.put('/groups/:id', authMiddleware, groupControllerInstance.updateGroup);
-groupRoutes.post('/groups/:id/insertUser/:userId', authMiddleware, groupControllerInstance.insertUser);
 groupRoutes.get('/groups/:id/users', authMiddleware, groupControllerInstance.getAllUsers);
 groupRoutes.delete('/groups/:id/removeUser/:userId', authMiddleware, groupControllerInstance.removeUser);
+
+// INVITES SERVICES
+groupRoutes.post('/group/:id/owner/:adminId/insertUser/:userId', authMiddleware, groupControllerInstance.acceptInvite);
+groupRoutes.post('/group/:id/user/:userId', authMiddleware, groupControllerInstance.sendInvite);
+groupRoutes.put('/group/:id/user/:user', authMiddleware, groupControllerInstance.rejectInvite);
+groupRoutes.get('/group/:id/owner/:adminId', authMiddleware, groupControllerInstance.getGroupsInvite);
 
 export { groupRoutes };
